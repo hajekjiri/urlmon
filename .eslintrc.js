773ed8b8df79fea875fbefddb284a221af2df911
Jiri Hajek <jiri.hajek18@gmail.com>
2020-08-18 17:37:39 +0200

    Remove eslint-plugin-node

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,21 +6,9 @@ module.exports = {
   ],
   extends: [
     'airbnb-typescript/base',
-    'plugin:node/recommended',
   ],
   parserOptions: {
     'ecmaVersion': 2020,
     'project': './tsconfig.json',
-  },
-  'rules': {
-    'node/exports-style': ['error', 'module.exports'],
-    'node/file-extension-in-import': ['error', 'always'],
-    'node/prefer-global/buffer': ['error', 'always'],
-    'node/prefer-global/console': ['error', 'always'],
-    'node/prefer-global/process': ['error', 'always'],
-    'node/prefer-global/url-search-params': ['error', 'always'],
-    'node/prefer-global/url': ['error', 'always'],
-    'node/prefer-promises/dns': 'error',
-    'node/prefer-promises/fs': 'error',
   },
 };
