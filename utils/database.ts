ea3153c0afa70531a88d81149b63d33337fdb90c
Jiri Hajek <jiri.hajek18@gmail.com>
2020-08-19 13:40:50 +0200

    Unify api and db environment variables

--- utils/database.ts
+++ utils/database.ts
@@ -4,12 +4,12 @@ let db: void | mysql.Connection;
 
 export async function initDbConnection(): Promise<void> {
   db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-  }).catch(async () => {
-    console.log('db connection failed, retrying in 5 seconds ...');
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  }).catch(async (e) => {
+    console.log(`db connection failed, retrying in 5 seconds ...\n${e}`);
     await new Promise((resolve) => { setTimeout(resolve, 5000); });
     await initDbConnection();
   });
