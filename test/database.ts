0525ef60baaa50bd7b78dba50b3ea1538ff9e89c
Jiri Hajek <jiri.hajek18@gmail.com>
2020-08-21 14:47:59 +0200

    Add database connection test

--- test/database.ts
+++ test/database.ts
@@ -1,9 +1,18 @@
 import dotenv from 'dotenv-safe';
+import { initDbConnection, getDbPool } from '../utils/database';
 
 describe('database', () => {
   describe('configuration', () => {
     it('should be stored in the .env file', () => {
       dotenv.config();
+    });
+  });
+  describe('connection', () => {
+    it('should be established', async () => {
+      dotenv.config();
+      await initDbConnection();
+      const pool = getDbPool();
+      pool.end();
     });
   });
 });
